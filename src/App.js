599a93fa3e9b396b9a4734ba6e0a577b7b8789de
Abdurrahman al djufry <Abdurrahmanaldjufry@gmail.com>
2022-12-06 14:13:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+// import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Navbars from "./navbar.jsx";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
 
 function App() {
+  const [data1, setData1] = useState([]);
+  const getData = () => {
+    Axios({
+      method: "get",
+      url: "https://api.themoviedb.org/3/movie/popular?api_key=e37c79c53f9d7b138c1790e896ed6cb1&language=en-",
+    }).then(function (response) {
+      setData1(response.data.results);
+    });
+  };
+  
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbars></Navbars>
+      <Container className="cardPlace">
+        <Row>
+          {data1.map((item, index) => {
+            return (
+              <Col key={index}>
+                <Card style={{ width: "20rem" }} className="cards">
+                  <Card.Img
+                    className="card-img-top"
+                    variant="top"
+                    src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                    alt="test"
+                    style={{ width: "100%" }}
+                  />
+                  <Card.Body className="card-body">
+                    <Card.Title className="card-title">{item.title}</Card.Title>
+                    <Card.Text className="card-text">
+                      movie id:{item.id}
+                    </Card.Text>
+                    <Card.Text className="card-text">{item.overview}</Card.Text>
+                    <Card.Text className="card-text">Watched: {parseInt(item.popularity)} time</Card.Text>
+
+                    <a
+                      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="btn mr-2"
+                    >
+                      <i className="bi bi-play-circle"></i> Trailer
+                    </a>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
