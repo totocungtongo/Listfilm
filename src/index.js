e6a51d2fd00c15957866af4aba4dfb1f731e7258
Abdurrahman al djufry <Abdurrahmanaldjufry@gmail.com>
2022-12-19 20:34:17 +0700

    change apple-icon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider,  } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"; 
 import Login from './Login.jsx';
 const router = createBrowserRouter([
   {
